0c0d3622a79a958e7e4db838cad7c371a79e3feb
vikas <vikaszende2004@gmail.com>
2025-08-31 02:58:07 +0530

    URL for frontend changed

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:5173',
+     origin: ["http://localhost:5173", "http://vikas-chat-appp.netlify.app"],
     credentials:true
 };
 app.use(cors(corsOption)); 
